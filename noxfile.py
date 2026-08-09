086e661523a3cde54ff4992ac529451b1962710f
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-08-10 01:25:30 +0200

    👷 Set up CI/CD

---
+++ noxfile.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env -S uv run --script --quiet
+# Copyright (c) 2026 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+# /// script
+# dependencies = ["nox"]
+# ///
+
+"""Nox sessions."""
+
+from __future__ import annotations
+
+import contextlib
+import os
+import shutil
+import tempfile
+from typing import TYPE_CHECKING
+
+import nox
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, Sequence
+
+
+nox.needs_version = ">=2025.10.16"
+nox.options.default_venv_backend = "uv"
+
+PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12", "3.13", "3.14"]
+
+if os.environ.get("CI", None):
+    nox.options.error_on_missing_interpreters = True
+
+
+@contextlib.contextmanager
+def preserve_lockfile() -> Generator[None]:
+    """Preserve the lockfile by moving it to a temporary directory."""
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        shutil.move("uv.lock", f"{temp_dir_name}/uv.lock")
+        try:
+            yield
+        finally:
+            shutil.move(f"{temp_dir_name}/uv.lock", "uv.lock")
+
+
+@nox.session(reuse_venv=True, default=True)
+def lint(session: nox.Session) -> None:
+    """Run the linter."""
+    if shutil.which("prek") is None:
+        session.install("prek")
+
+    session.run("prek", "run", "--all-files", *session.posargs, external=True)
+
+
+def _run_tests(
+    session: nox.Session,
+    *,
+    install_args: Sequence[str] = (),
+    extra_command: Sequence[str] = (),
+    pytest_run_args: Sequence[str] = (),
+) -> None:
+    env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+
+    if extra_command:
+        session.run(*extra_command, env=env)
+    session.run(
+        "uv",
+        "run",
+        "--no-dev",
+        "--group",
+        "test",
+        *install_args,
+        "pytest",
+        *pytest_run_args,
+        *session.posargs,
+        "--cov-config=pyproject.toml",
+        env=env,
+    )
+
+
+@nox.session(python=PYTHON_ALL_VERSIONS, reuse_venv=True, default=True)
+def tests(session: nox.Session) -> None:
+    """Run the test suite."""
+    _run_tests(session)
+
+
+@nox.session(python=PYTHON_ALL_VERSIONS, reuse_venv=True, venv_backend="uv", default=True)
+def minimums(session: nox.Session) -> None:
+    """Test the minimum versions of dependencies."""
+    with preserve_lockfile():
+        _run_tests(
+            session,
+            install_args=["--resolution=lowest-direct"],
+            pytest_run_args=["-Wdefault"],
+        )
+        env = {"UV_PROJECT_ENVIRONMENT": session.virtualenv.location}
+        session.run("uv", "tree", "--frozen", env=env)
+
+
+if __name__ == "__main__":
+    nox.main()
---
+++ tests/test_temp.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2026 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Temporary test to test the CI."""
+
+
+def test_temp() -> None:
+    """Temporary test to test the CI."""
+    assert True
